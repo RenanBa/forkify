bcd68919f29e7d524c057cbdbf585e314b299477
RenanBa <renanbarbosadesouza@gmail.com>
2020-05-21 14:36:19 -0700

    Build the search view part 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,37 @@
-import axios from 'axios';
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-async function getResults(query) {
-    const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes)
-    console.log(res.data)
+/** Global state of the app will store:
+ *  - Search object
+ *  - Current recipe object
+ *  - Shopping list object
+ *  - Liked recipes 
+ */ 
+const state = {}; // global state
+
+const controlSearch = async () => { // using 'async' function to be able to use 'await' 
+    // 1) get query from the view
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2) new search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for result
+        searchView.clearInput();
+        searchView.clearResults();
+        // 4) Search for recipes
+        await state.search.getResults(); // wait for promise to return to then proceed
+
+        // 5) Render results on UI
+        searchView.renderResults(state.search.result);
+    }
 }
-getResults("pizza");
+
+elements.searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+    controlSearch();
+})
+
+
