8d3887fcbce80f0235e757b79931f129cd8d8396
RenanBa <renanbarbosadesouza@gmail.com>
2020-05-22 09:21:57 -0700

    Implement search result pagination

--- src/js/index.js
+++ src/js/index.js
@@ -35,6 +35,15 @@ const controlSearch = async () => { // using 'async' function to be able to use 
 elements.searchForm.addEventListener('submit', event => {
     event.preventDefault();
     controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
 })
 
 
