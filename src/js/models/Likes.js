45c78234affc8201c0544d959503421a0be00cdc
RenanBa <renanbarbosadesouza@gmail.com>
2020-05-25 09:06:26 -0700

    Implement persistent data for liked recipes

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -6,6 +6,10 @@ export default class Likes {
     addLike(id, title, author, img) {
         const like = { id, title, author, img };
         this.likes.push(like);
+        
+        // Persist data in local storage (localStorage)
+        this.persistData();
+
         return like;
     }
 
@@ -21,5 +25,18 @@ export default class Likes {
 
     getNumLikes() {
         return this.likes.length;
+    }
+
+    persistData() {
+        // localStorage only deals with strings. That is why the this.likes are parsed to string
+        localStorage.setItem('likes', JSON.stringify(this.likes));
+    }
+
+    readStorage() {
+        // conver the string back to the structure before being stringigy
+        const storage = JSON.parse(localStorage.getItem('likes'));
+        
+        // now we restore the likes from localStorage to the this.likes object
+        if (storage) this.likes = storage;
     }
 }
